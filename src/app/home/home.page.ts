a0e12bcd7490da3b6d171c766a68d12c538527a2
mc-perry <jonathan@m-craft.com>
2020-09-28 18:22:14 +0900

    Successfully caused compile of app with errors pushing data to amplify

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { APIService } from '../API.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  todos: Array<any>;
 
-  constructor() {}
+  constructor(private apiService: APIService) {
+    this.apiService.OnCreateTodoListener.subscribe(evt => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+      console.log('this todos is: ', this.todos);
+    });
+  }
 
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'ionic',
+      description: 'testing'
+    });
+  }
 }
